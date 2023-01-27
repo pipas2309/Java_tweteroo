a18199197a72af89fef7c12723e7535326cf19ae
Lucas Palharini <lucas230991@hotmail.com>
2023-01-27 01:42:21 -0300

    :sparkles: feat: validation in dto

--- src/main/java/com/tweteroo/api/dto/UserDTO.java
+++ src/main/java/com/tweteroo/api/dto/UserDTO.java
@@ -1,9 +1,15 @@
 package com.tweteroo.api.dto;
 
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 public record UserDTO(
 
-    String username,
-    String avatar 
-    
-) {     
+        @NotBlank String username,
+
+        @URL @NotEmpty String avatar
+
+) {
 }
