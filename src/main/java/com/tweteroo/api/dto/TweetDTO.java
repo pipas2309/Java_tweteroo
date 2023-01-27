b1c5d6d244339b94a581c2f236c023949b0147ce
Lucas Palharini <lucas230991@hotmail.com>
2023-01-27 01:57:03 -0300

    :sparkles: feat: update tweetDTO
    now has a validation

--- src/main/java/com/tweteroo/api/dto/TweetDTO.java
+++ src/main/java/com/tweteroo/api/dto/TweetDTO.java
@@ -1,10 +1,18 @@
 package com.tweteroo.api.dto;
 
-public record TweetDTO(
-    
-    String username,
-    String avatar,
-    String text
+import org.hibernate.validator.constraints.URL;
 
-) {    
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record TweetDTO(
+
+        @NotBlank @Size(max = 42) String username,
+
+        @NotNull @URL @Size(max = 200) String avatar,
+
+        @NotBlank @Size(max = 144) String text
+
+) {
 }
