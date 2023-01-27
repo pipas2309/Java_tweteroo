b7ed96312fee3a4079b8cf7cc46463f8e8ae441d
Lucas Palharini <lucas230991@hotmail.com>
2023-01-27 00:11:22 -0300

    :sparkles: feat: update userModel
    now the UserModel is ready to be used in h2database.

--- src/main/java/com/tweteroo/api/model/UserModel.java
+++ src/main/java/com/tweteroo/api/model/UserModel.java
@@ -1,12 +1,33 @@
 package com.tweteroo.api.model;
 
+import com.tweteroo.api.dto.UserDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @NoArgsConstructor
 public class UserModel {
+
+    public UserModel(UserDTO user) {
+        this.username = user.username();
+        this.avatar = user.avata();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 42, nullable = false)
     private String username;
+
+    @Column(length = 200, nullable = false)
     private String avatar;
 }
 
