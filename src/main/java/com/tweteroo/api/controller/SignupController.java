ffe0fae9c6f6383585e66a4693e05617ae009091
Lucas Palharini <lucas230991@hotmail.com>
2023-01-27 01:45:18 -0300

    :construction: feat: create post endpoint
    function create() now has access to the repository

--- src/main/java/com/tweteroo/api/controller/SignupController.java
+++ src/main/java/com/tweteroo/api/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,14 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.UserDTO;
+import com.tweteroo.api.model.UserModel;
+import com.tweteroo.api.repository.UserRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class SignupController {
     
+    @Autowired
+    private UserRepository repository;
+
     @CrossOrigin
     @PostMapping("/auth/sign-up")
-    public ResponseEntity<String> create(@RequestBody UserDTO user) {
-        return ResponseEntity.status(202).body("OK");
+    public ResponseEntity<String> create(@Valid @RequestBody UserDTO user) {
+
+        repository.save(new UserModel(user));
+
+        return ResponseEntity.status(201).body("OK");
     } 
 }
