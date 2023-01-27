386ade07156490cfe825848ab1e72fbd8402520d
Lucas Palharini <lucas230991@hotmail.com>
2023-01-27 00:02:12 -0300

    :bug: fix: add cors permission

--- src/main/java/com/tweteroo/api/controller/SignupController.java
+++ src/main/java/com/tweteroo/api/controller/SignupController.java
@@ -1,6 +1,7 @@
 package com.tweteroo.api.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.tweteroo.api.dto.UserDTO;
 @RequestMapping("/api")
 public class SignupController {
     
+    @CrossOrigin
     @PostMapping("/auth/sign-up")
     public ResponseEntity<String> create(@RequestBody UserDTO user) {
         return ResponseEntity.status(202).body("OK");
