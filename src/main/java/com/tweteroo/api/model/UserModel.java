42ee1b3852fab2e8eb1b169b61df2539e3afae9c
Lucas Palharini <lucas230991@hotmail.com>
2023-01-26 23:31:35 -0300

    :sparkles: feat: create UserModel

---
+++ src/main/java/com/tweteroo/api/model/UserModel.java
@@ -0,0 +1,12 @@
+package com.tweteroo.api.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserModel {
+    private String username;
+    private String avatar;
+}
+
